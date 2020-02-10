b8b2e9857220cf2466e36d5fa016180503f69fe7
Finn Vamosi <43504390+Liteknight@users.noreply.github.com>
2020-02-09 21:26:28 -0700

    Add files via upload

---
+++ Zoma.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class Zoma {
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		int n = in.nextInt();
+		String s = in.next();
+		
+		System.out.println(n + 1);
+		in.close();
+	}
+}
