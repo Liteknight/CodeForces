208d2226796e4a84cd06b6932b650c3a43f4ab39
Finn Vamosi <43504390+Liteknight@users.noreply.github.com>
2020-04-15 15:39:04 -0600

    Add files via upload

---
+++ UltraFastMathematician.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class UltraFastMathematician {
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+		String s1 = in.next(), s2 = in.next();
+
+		for (int i = 0; i < s1.length(); i++) {
+			
+			if (s1.charAt(i) == s2.charAt(i)) {
+				System.out.print("0");
+				
+			} else {
+				System.out.print("1");
+			}
+		}
+		in.close();
+	}
+}
