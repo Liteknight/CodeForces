5013bbf382d939f3ed70a80c4fff8a71a1f623f2
Finn Vamosi <43504390+Liteknight@users.noreply.github.com>
2020-05-09 11:15:37 -0600

    Add files via upload

---
+++ Again25.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Again25 {
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+		@SuppressWarnings("unused")
+		long n = in.nextLong();
+		in.close();
+		
+		System.out.println(25);
+	}
+}
