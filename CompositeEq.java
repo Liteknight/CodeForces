d2e315e465dc41b35597f0914e7e9ff74c195c76
Finn Vamosi <43504390+Liteknight@users.noreply.github.com>
2020-06-03 15:19:28 -0600

    Add files via upload

---
+++ CompositeEq.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class CompositeEq {
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+
+		in.close();
+		System.out.println(n * 9 + " " + n * 8);
+	}
+}
