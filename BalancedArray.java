44f92bd3fc056707511c2c1b6a502e84940910f3
Finn Vamosi <43504390+Liteknight@users.noreply.github.com>
2020-06-23 12:54:13 -0600

    Add files via upload

--- BalancedArray.java
+++ BalancedArray.java
@@ -4,19 +4,19 @@ public class BalancedArray {
 	public static void main(String[] args) {
 
 		Scanner in = new Scanner(System.in);
-		
+
 		int t = in.nextInt();
 		while (t-- > 0) {
 
 			long n = in.nextLong(), i = 2;
-			String oe = "", oo = "";
+			StringBuilder oe = new StringBuilder(), oo = new StringBuilder();
 
 			System.out.println(n % 4 == 0 ? "YES" : "NO");
 			if (n % 4 == 0) {
 				
 				while (i < n - 1) {
-					oe += i + " ";
-					oo += (i - 1) + " ";
+					oe.append(i + " ");
+					oo.append((i - 1) + " ");
 					i += 2;
 				}
 				System.out.printf("%s%d %s%d\n", oe, i, oo, i + n / 2 - 1);
