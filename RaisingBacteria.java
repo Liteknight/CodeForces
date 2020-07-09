dbc8ab91693f90124b0684814f0b46ec2998a0ef
Finn Vamosi <43504390+Liteknight@users.noreply.github.com>
2020-07-09 14:08:47 -0600

    Add files via upload

---
+++ RaisingBacteria.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class RaisingBacteria {
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+		System.out.println(Integer.bitCount(in.nextInt()));
+		in.close();
+	}
+}
