d44092ab0e3dd69bb28947bd4a823e64da046e6b
Finn Vamosi <43504390+Liteknight@users.noreply.github.com>
2020-11-17 20:25:26 -0700

    Add files via upload

---
+++ HexaTheorem.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class HexaTheorem {
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+		long n = in.nextLong();
+
+		in.close();
+		System.out.println("0 0 " + n);
+	}
+}
